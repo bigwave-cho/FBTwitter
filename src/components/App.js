ceb5ee2fb47df3b903394590c5e4ca364ef1186f
bigwave-cho <hooyaa93@gmail.com>
2023-03-12 15:52:09 +0900

    firebase Auth setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import AppRouter from './Router';
+import { useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
+//https://create-react-app.dev/docs/importing-a-component/#absolute-imports
+//jsconfig.json에 컴파일러 옵션의 baseUrl을 src로 설정하면
+//import시 src부터 찾기 때문에 ../  <- 요거 없애기 가능. (절대경로)
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // user || null
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;

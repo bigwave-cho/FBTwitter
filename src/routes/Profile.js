3564ce8f5d6fdb78cf02a488e816ac0331442907
bigwave-cho <hooyaa93@gmail.com>
2023-03-12 17:19:12 +0900

    Logout with redirect

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
+import { authService } from 'fbase';
+import { useHistory } from 'react-router-dom';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  // redircet 시키는 방법 2
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
 };
 
 export default Profile;

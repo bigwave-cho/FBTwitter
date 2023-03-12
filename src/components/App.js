339d318b54b10f0766bae691204c639b18993d99
bigwave-cho <hooyaa93@gmail.com>
2023-03-12 16:47:20 +0900

    signup & in feature(onAuthStateChanged)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    //https://firebase.google.com/docs/auth/web/manage-users?hl=ko
+    //onAuthStateChagned : auth 상태 확인 메서드
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
       <footer>&copy; {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -9,16 +9,8 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAccount, setNewAccount] = useState(true);
-
+  const [error, setError] = useState('');
   const onChange = (event) => {
-    // sign with email... 에 관한 doc
-    //https://firebase.google.com/docs/reference/js/auth.auth?hl=ko
-
-    // persistence에 대한 doc (로그인 지속)
-    //https://firebase.google.com/docs/auth/web/auth-state-persistence?hl=ko
-    //default는 local이며 브라우저를 꺼도 지속되므로 직접 로그아웃 절차를 거쳐야 함.
-
-    // ## 로그인 여부는 애플리케이션 -> indexDB -> firebaseLocalStorageDb에서 확인
     const {
       target: { name, value },
     } = event;
@@ -47,8 +39,13 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
+      //Firebase: Error (auth/email-already-in-use). 같은 메일로 생성 시 에러
     }
+  };
+
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
   };
 
   return (
@@ -71,7 +68,11 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? 'Create Account' : 'Log in'} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'sign in' : 'Create Account'}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>

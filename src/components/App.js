73ce25d56fbf7db96018cf45c7f299cad527d360
bigwave-cho <hooyaa93@gmail.com>
2023-03-15 14:45:32 +0900

    Auth : nav 이름 변경 안되는 bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -14,15 +14,35 @@ function App() {
             displayName: 'NoName',
           });
         }
+        // 방법 1.
+        // setUserObj({
+        //   displayName: user.displayName,
+        //   uid: user.uid,
+        //   updateProfile: (args) => user.updateProfile(args),
+        // });
+
+        //방법2. 그대로 두고.
         setUserObj(user);
       } else {
       }
       setInit(true);
     });
   }, [userObj]);
-
+  //## 이름 변경해도 Nav가 즉시 렌더링되지 않는 버그를 고쳐보자.
+  // 상황 : 리액트가 스테이트의 변경을 감지하지 못하고 있음.
   const refreshUser = () => {
-    setUserObj(authService.displayName);
+    const user = authService.currentUser;
+    // 방법1.
+    // setUserObj({
+    //   displayName: user.displayName,
+    //   uid: user.uid,
+    //   updateProfile: (args) => user.updateProfile(args),
+    // });
+
+    // 방법2.
+    // Objcet.assign(target, source) // 빈 객체에 source 사본을 할당
+    // 리액트가 객체가 변경됐음을 감지하게 됨.
+    setUserObj(Object.assign({}, user));
   };
 
   return (
